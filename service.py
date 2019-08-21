9d41f346af989a80e35350cb422b9718fd01e224
Peziman <44964725+Peziman@users.noreply.github.com>
2019-08-21 16:05:56 +0200

    Update service.py

--- service.py
+++ service.py
@@ -15,12 +15,12 @@ OUT_BACKUP_IGN_PIN	= 22	#Uberbruckung Herrunterfahren
 OUT_PWR_DISPLAY		= 24	#Display Hintergrundbeleuchtung -> Soll nach gewisser Zeit ausschalten wenn in uberbruckung
 
 class Main(object):
-	ignore_ign = False		
-	run = True		#BOOL thread lauft
-	stopped = False		#BOOL thread beendet wurde (ob ich das brauche?) 
+	ignore_ign = False	#BOOL Ignoriere Herrunterfahren	
+	run = True		#BOOL thread soll laufen
+	stopped = False		#BOOL thread beendet 
 	power_dialog = None	#Meldefenster in Kodi
 	power_display = True	#BOOL Display AN
-	rear_thread = NONE	#Ich werde wohl eine Thread brauchen wegen der Rückfahrkamera
+	monitor = xbmc.Monitor()
 	
 	def __init__(self):
 		pass
@@ -29,16 +29,47 @@ class Main(object):
 		self.thread = Thread(target=self.gpio_checker)
 		self.thread.setDaemon(True)
 		self.thread.start()
+		monitor.waitForAbort() #laut Wiki bleibt hier die Schleife stehen bis Kodi beendet wird
+		run = False
+		#Hier konnte man noch eine Resume-Funktion einfugen
+		if stopped == True: #warten bis thread beendet ist und danach weiter
+			GPIO.cleanup()
+			xbmc.log("Service CarPCButler beendet! %s" %time.time(), level=xbmc.LOGNOTICE)
+		
 		
 	def gpio_checker(self):	
+		xbmc.log("CarPCButler: Checker Thread started! %s" %time.time(), level=xbmc.LOGNOTICE)
 		GPIO.setmode(GPIO.BCM)
+		#Setup der GPIOs
+		#Inputs
+		GPIO.setup(IN_IGN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(IN_REVERSE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(IN_LIGHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		#Outputs
+		GPIO.setup(OUT_BACKUP_IGN_PIN, GPIO.OUT)
+		GPIO.output(OUT_BACKUP_IGN_PIN, 1)
+		GPIO.setup(OUT_PWR_DISPLAY, GPIO.OUT)
+		GPIO.output(OUT_PWR_DISPLAY, 1)
+		GPIO.setup(OUT_LED_RUN_PIN, GPIO.OUT)
+		GPIO.output(OUT_LED_RUN_PIN, 0)
 		
-		#Hier gehts weiter!!!
+		while self.run:
+			power = GPIO.input(IN_IGN_PIN)
+			if power == True:
+				GPIO.output(OUT_LED_RUN_PIN, 1)
+				self.power_is_on()	#!!!def erstellen!!!
+			elif power == False:
+				xbmc.log("Car IGN turned off! %s" %time.time(), level=xbmc.LOGNOTICE)
+				self.power_is_off()	#!!!def erstellen!!!
+			time.sleep(0.1)
+		
+		self.stopped = True
 
 
 
 if __name__ == '__main__':
 	dialog = xbmcgui.Dialog()
+	xbmc.log("Service CarPCButler started! %s" %time.time(), level=xbmc.LOGNOTICE)
 	main = Main()
 	main.start()
 	
