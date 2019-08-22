1135c7567ba74b4e66bfcc4fa89066fb7e510547
Peziman <44964725+Peziman@users.noreply.github.com>
2019-08-22 14:33:35 +0200

    Update service.py

--- service.py
+++ service.py
@@ -20,6 +20,7 @@ class Main(object):
 	stopped = False		#BOOL thread beendet 
 	power_dialog = None	#Meldefenster in Kodi
 	power_display = True	#BOOL Display AN
+	option_rearcam = True	#Funktion Ruckfahrkamera aktiv
 	monitor = xbmc.Monitor()
 	wait_time = 600
 	
@@ -60,9 +61,12 @@ class Main(object):
 				GPIO.output(OUT_LED_RUN_PIN, 1)
 				self.power_is_on()	
 			elif power == False:
-				xbmc.log("Car IGN turned off! %s" %time.time(), level=xbmc.LOGNOTICE)
+				xbmc.log("CarPCButler: Car IGN turned off! Shut down will be prepared... %s" %time.time(), level=xbmc.LOGNOTICE)
 				self.power_is_off()	
+			self.rearcam
+			
 			time.sleep(0.1)
+
 		
 		self.stopped = True
 		
@@ -75,36 +79,47 @@ class Main(object):
 	def power_is_off(self):
 		self.power_dialog = xbmcgui.Dialog()
 		self.power_dialog_create = power_dialog.yesno("CarPCButler", "Zündung ist Aus!", "Machen sie einen Tankstopp?", "Soll ich auf Sie warten?", "Ja Warte", "Nein", 10000) #Stringdatei für andere Sprachen erstellen
+		self.ignore_ign = TRUE
 		power_back = FALSE
-		shut_it_down = FALSE
 		
 		if self.power_dialog == TRUE: #Wenn "Ja warte" gedruckt wird warte 10Min mit dem herrunterfahren
+			xbmc.log("shut down paused by User! %s" %time.time(), level=xbmc.LOGNOTICE)
 			#self.power_dialog.close()
 			self.power_dialog = NONE
 			GPIO.output(OUT_PWR_DISPLAY, 0)
 			p = 0
 			while self.wait_time < p:
 				if GPIO.input(IN_IGN_PIN) == TRUE: #Wenn die Zundung innerhalb der 10Min wieder da ist, breche die Schleife ab
+					xbmc.log("CarPCButler: Car IGN turn on! %s" %time.time(), level=xbmc.LOGNOTICE)
 					power_back = TRUE
 					break
 				else:
-					p = p +1
+					p = p + 1
 					time.sleep(1)
 			if power_back == FALSE:
 				self.shut_down
 			else:
 				GPIO.output(OUT_PWR_DISPLAY, 1)
+				self.ignore_ign = FALSE
 				wb_dialog = xbmc.Dialog()
-				wb_dialog.notification('CarPCButler', 'Willkommen zuück!', xbmcgui.NOTIFICATION_INFO, 5000)
+				wb_dialog.notification('CarPCButler', 'Willkommen zuück!', xbmcgui.NOTIFICATION_INFO, 5000) #Stringdatei für andere Sprachen erstellen
 				
-		elif self.power_dialog == FALSE and not power_back: #Wenn "Nein" gedruckt wird, fahre das System sofort herunter
+		elif self.power_dialog == FALSE and power_back == TRUE: #Wenn "Nein" gedruckt wird, fahre das System sofort herunter
 			self.shut_down
 					
 	def shut_down(self):
-		xbmc.log("CarPCButler turn off Pi! %s" %time.time(), level=xbmc.LOGWARNING)
+		xbmc.log("CarPCButler: Shut down Pi! %s" %time.time(), level=xbmc.LOGWARNING)
 		os.system("sudo shutdown -h now")
 		GPIO.output(OUT_BACKUP_IGN_PIN, 0)
 		GPIO.output(OUT_PWR_DISPLAY, 0)
+		
+	def rearcam(self):
+		if xbmc.getCondVisibility('System.HasAddon(%s)' %plugin.program.rearcam) == 1: #Konrolle ob das plugin so heisst
+			#Hier mit Rearcam funktion weitermachen
+		else:
+			fault_dialog = xbmc.Dialog()
+			fault_dialog.notification('CarPCButler', 'Rearcam PlugIn nicht installiert!', xbmcgui.NOTIFICATION_ERROR, 5000)
+			xbmc.log("CarPCButler: Rearcam Plugin not installed! Please install the Plugin for more information visit www,raspicar-projekt.de %s" %time.time(), level=xbmc.LOGWARNING)
 
 
 if __name__ == '__main__':
