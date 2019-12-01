34a2b904a5da96616047ca65a5afcb1df1d79ff9
Peziman <44964725+Peziman@users.noreply.github.com>
2019-12-01 16:16:40 +0100

    Update service.py

--- service.py
+++ service.py
@@ -1,30 +1,33 @@
 import os, time
 import xbmc, xbmcgui, xbmcaddon
 import RPi.GPIO as GPIO
+import subprocess
 from threading import Thread
 
 __addon__	= xbmcaddon.Addon()
 __addonid__	= __addon__.getAddonInfo('id').decode( 'utf-8' )
 __addonname__	= __addon__.getAddonInfo('name').decode("utf-8")
+__addonpath__ = __addon__.getAddonInfo('path').decode("utf-8")
+addon = xbmcaddon.Addon(id='plugin.service.carpcButler')
 
-OUT_LED_RUN_PIN		= 14	#Anzeige Pi lauft  
-IN_IGN_PIN		= 16	#Signal Zundung ist an
-IN_LIGHT_PIN		= 17	#Signal Dammerungsschalter
-IN_REVERSE_PIN		= 18	#Signal Ruckwartsgang
-OUT_BACKUP_IGN_PIN	= 20	#Uberbruckung Herrunterfahren
-OUT_PWR_DISPLAY		= 24	#Display Hintergrundbeleuchtung -> Soll nach gewisser Zeit ausschalten wenn in uberbruckung
+OUT_LED_RUN_PIN		= int(addon.getSetting('out_led_run_pin'))		#Anzeige Pi lauft  
+IN_IGN_PIN			= int(addon.getSetting('in_ign_pin'))			#Signal Zundung ist an
+IN_LIGHT_PIN		= int(addon.getSetting('in_light_pin'))			#Signal Dammerungsschalter
+IN_REVERSE_PIN		= int(addon.getSetting('in_reverse_pin'))		#Signal Ruckwartsgang
+OUT_BACKUP_IGN_PIN	= int(addon.getSetting('out_backup_ign_pin'))	#Ausgang Uberbruckung Spannungsversorgung
+OUT_PWR_DISPLAY		= int(addon.getSetting('out_pwr_display'))		#Display Hintergrundbeleuchtung -> Soll nach gewisser Zeit ausschalten wenn in uberbruckung
+OUT_AMP_CONTROL		= int(addon.getSetting('out_amp_control'))		#Ausgang fur Verstarker remote
 
 class Main(object):
-	ignore_ign = False	#BOOL Ignoriere Herrunterfahren	
-	run = True		#BOOL thread soll laufen
-	stopped = False		#BOOL thread beendet 
-	power_dialog = None	#Meldefenster in Kodi
+	ignore_ign = False				#BOOL Ignoriere Herrunterfahren	
+	run = True						#BOOL thread soll laufen
+	stopped = False					#BOOL thread beendet 
+	power_dialog = None				#Meldefenster in Kodi
 	power_dialog_pass = False
-	power_display = True	#BOOL Display AN
-	option_rearcam = True	#BOOL Funktion Ruckfahrkamera aktiv
-	rearcam_trigger = False	#BOOL Ruckfahrkamera ist aktiv
-	lightswitch_trigger = False	#BOOL Licht ist aktiv
-	wait_time = 600			#INT Wartezeit bei Tankstop in Sekunden
+	power_display = True			#BOOL Display AN
+	rearcam_trigger = False			#Ruckfahrkamera ist aktiv
+	lightswitch_trigger = False		#Licht ist aktiv
+	wait_time = int(addon.getSetting('wait_time'))		#Wartezeit bei Tankstop in Sekunden
 	laststate_play = False
 	
 	def __init__(self):
@@ -34,13 +37,14 @@ class Main(object):
 		self.thread = Thread(target=self.gpio_checker)
 		self.thread.setDaemon(True)
 		self.thread.start()
+		self.output_control("amp_control", "on", OUT_AMP_CONTROL)
 		monitor = xbmc.Monitor()
 		monitor.waitForAbort() #laut Wiki bleibt hier die Schleife stehen bis Kodi beendet wird
 		self.run = False
 		#Hier konnte man noch eine Resume-Funktion einfugen
-		if self.stopped == True: #warten bis thread beendet ist und danach weiter
-			GPIO.cleanup()
-			xbmc.log("Service CarPCButler stopped! %s" %time.time(), level=xbmc.LOGNOTICE)
+		self.thread.join()
+		GPIO.cleanup()
+		xbmc.log("Service CarPCButler stopped! %s" %time.time(), level=xbmc.LOGNOTICE)
 		
 		
 	def gpio_checker(self):	
@@ -53,27 +57,27 @@ class Main(object):
 		GPIO.setup(IN_LIGHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 		#Outputs
 		GPIO.setup(OUT_BACKUP_IGN_PIN, GPIO.OUT)
-		GPIO.output(OUT_BACKUP_IGN_PIN, GPIO.HIGH)
 		GPIO.setup(OUT_PWR_DISPLAY, GPIO.OUT)
-		GPIO.output(OUT_PWR_DISPLAY, GPIO.HIGH)
 		GPIO.setup(OUT_LED_RUN_PIN, GPIO.OUT)
-		GPIO.output(OUT_LED_RUN_PIN, GPIO.LOW)
+		GPIO.setup(OUT_AMP_CONTROL, GPIO.OUT)
 		
 		while self.run:
 			power = GPIO.input(IN_IGN_PIN)
 			if power == True:
-				GPIO.output(OUT_LED_RUN_PIN, GPIO.HIGH)
+				self.output_control("option_power_led", "on", OUT_LED_RUN_PIN)
 				self.power_is_on()	
 			elif power == False and self.power_dialog_pass == False:
-				GPIO.output(OUT_LED_RUN_PIN, GPIO.LOW)
+				self.output_control("option_power_led", "off", OUT_LED_RUN_PIN)
 				xbmc.log("CarPCButler: Car IGN turned off! Shut down will be prepare... %s" %time.time(), level=xbmc.LOGNOTICE)
 				self.checkplayer()
 				self.power_is_off()
 			else:
 				pass
 			
-			self.rearcam()
-			self.daynight()
+			if str(addon.getSetting('option_rearcam')) == "true":
+				self.rearcam()
+			if str(addon.getSetting('option_light')) == "true":
+				self.daynight()
 			
 			time.sleep(0.1)
 
@@ -88,17 +92,15 @@ class Main(object):
 			self.power_dialog = None
 	
 	def power_is_off(self):
-		#self.pwr_dialog = xbmcgui.Dialog()
 		power_dialog = xbmcgui.Dialog().yesno("$ADDON[plugin.service.carpcButler 30000]", "$ADDON[plugin.service.carpcButler 30001]", "$ADDON[plugin.service.carpcButler 30002]", "$ADDON[plugin.service.carpcButler 30003]", autoclose = 10000) 
 		self.ignore_ign = True
 		self.power_dialog_pass = True
 		self.power_back = False
 		
 		if power_dialog: #Wenn "Ja warte" gedruckt wird warte 10Min mit dem herrunterfahren
-			xbmc.log("CarPCButler: shut down paused by User! %s" %time.time(), level=xbmc.LOGNOTICE)
-			#self.power_dialog.close()
-			#self.power_dialog = None
-			GPIO.output(OUT_PWR_DISPLAY, GPIO.LOW)
+			xbmc.log("shut down paused by User! %s" %time.time(), level=xbmc.LOGNOTICE)
+			self.display_control("off")
+			self.output_control("amp_control", "off", OUT_AMP_CONTROL)
 			p = 0
 			while self.wait_time > p:
 				if GPIO.input(IN_IGN_PIN) == True: #Wenn die Zundung innerhalb der 10Min wieder da ist, breche die Schleife ab
@@ -107,17 +109,15 @@ class Main(object):
 					self.power_is_on()
 					break
 				else:
-					if GPIO.input(OUT_LED_RUN_PIN) == True:
-						GPIO.output(OUT_LED_RUN_PIN, GPIO.LOW)
-					else:
-						GPIO.output(OUT_LED_RUN_PIN, GPIO.HIGH)
+					self.output_control("option_power_led", "toggle", OUT_LED_RUN_PIN)
 					p = p + 1
 				time.sleep(1)
 			if self.power_back == False:
-				xbmc.log("CarPCButler: Timeout.... shutdown! %s" %time.time(), level=xbmc.LOGNOTICE)
+				xbmc.log("Timeout.... shutdown! %s" %time.time(), level=xbmc.LOGNOTICE)
 				self.shut_down()
 			else:
-				GPIO.output(OUT_PWR_DISPLAY, GPIO.HIGH)
+				self.display_control("on")
+				self.output_control("amp_control", "on", OUT_AMP_CONTROL)
 				self.ignore_ign = False
 				wb_dialog = xbmcgui.Dialog()
 				wb_dialog.notification("$ADDON[plugin.service.carpcButler 30000]", "$ADDON[plugin.service.carpcButler 30006]", xbmcgui.NOTIFICATION_INFO, 5000)
@@ -125,16 +125,17 @@ class Main(object):
 				
 		else:		#Wenn "Nein" gedruckt wird, fahre das System sofort herunter
 			if GPIO.input(IN_IGN_PIN) == False: #Wenn die Zundung sofort wieder da ist schalte CarPC nicht ab.
-				xbmc.log("CarPCButler: Shut down by User! %s" %time.time(), level=xbmc.LOGNOTICE)
+				xbmc.log("shut down by User! %s" %time.time(), level=xbmc.LOGNOTICE)
 				self.shut_down()
 			else:
 				pass
 					
 	def shut_down(self):
 		xbmc.log("CarPCButler: Shut down Pi! %s" %time.time(), level=xbmc.LOGWARNING)
+		self.output_control("amp_control", "off", OUT_AMP_CONTROL)
+		self.display_control("off")
 		os.system("sudo shutdown -h now")
-		#GPIO.output(OUT_BACKUP_IGN_PIN, GPIO.LOW)
-		GPIO.output(OUT_PWR_DISPLAY, GPIO.LOW)
+
 		
 	def rearcam(self):
 		if xbmc.getCondVisibility('System.HasAddon(plugin.program.pidash)') == 1:
@@ -159,6 +160,7 @@ class Main(object):
 			fault_dialog = xbmcgui.Dialog()
 			fault_dialog.notification("$ADDON[plugin.service.carpcButler 30000]", "$ADDON[plugin.service.carpcButler 30007]", xbmcgui.NOTIFICATION_ERROR, 5000)
 			xbmc.log("CarPCButler: Plugin piDash not installed! Please install the Plugin. For more information visit https://raspicarprojekt.de/showthread.php?tid=861 %s" %time.time(), level=xbmc.LOGWARNING)
+			addon.setSetting(id='option_rearcam', value='false')
 
 	def daynight(self):
 		if xbmc.getCondVisibility('System.HasAddon(plugin.program.carpc-xtouch)') == 1:
@@ -186,6 +188,7 @@ class Main(object):
 			fault_dialog = xbmcgui.Dialog()
 			fault_dialog.notification("$ADDON[plugin.service.carpcButler 30000]", "$ADDON[plugin.service.carpcButler 30008]", xbmcgui.NOTIFICATION_ERROR, 5000)
 			xbmc.log("CarPCButler: Plugin xTouch not installed! Please install the Plugin. For more information visit https://raspicarprojekt.de/showthread.php?tid=913 %s" %time.time(), level=xbmc.LOGWARNING)
+			addon.setSetting(id='option_light', value='false')
 			
 	def checkplayer(self):
 		player = xbmc.Player()
@@ -202,7 +205,33 @@ class Main(object):
 			player.pause()
 		else:
 			pass	
-					  
+			
+	def output_control(self, output_setting, output_mode, output_no):
+		if str(addon.getSetting(output_setting)) == "true":
+			if output_mode == "on" and GPIO.input(output_no) == False:
+				GPIO.output(output_no, GPIO.HIGH)
+			if output_mode == "off" and GPIO.input(output_no) == True:
+				GPIO.output(output_no, GPIO.LOW)
+			if output_mode == "toggle" and GPIO.input(output_no) == True:
+				GPIO.output(output_no, GPIO.LOW)
+			if output_mode == "toggle" and GPIO.input(output_no) == False:
+				GPIO.output(output_no, GPIO.HIGH)
+		else:
+			pass
+		
+	def display_control(self, display_mode):
+		if str(addon.getSetting('display_hard_mode')) == "true":
+			if display_mode == "off" and GPIO.input(OUT_PWR_DISPLAY) == True:
+				GPIO.output(OUT_PWR_DISPLAY, GPIO.LOW)
+			if display_mode == "on" and GPIO.input(OUT_PWR_DISPLAY) == False:
+				GPIO.output(OUT_PWR_DISPLAY, GPIO.HIGH)
+		else:
+			if display_mode == "off" and self.power_back == False:
+				subprocess.call(["vcgencmd", "display_power", "0"])
+			if display_mode == "on" and self.power_back == True:
+				subprocess.call(["vcgencmd", "display_power", "1"])
+
+	
 if __name__ == '__main__':
 	xbmc.log("Service CarPCButler started! %s" %time.time(), level=xbmc.LOGNOTICE)
 	main = Main()
